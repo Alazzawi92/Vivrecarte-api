b036d77d34ac1e61d39751ffc74de8f956cb3a50
wazabi64000 <wazabi64000@gmail.com>
2026-02-28 11:46:32 +0100

    route get

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,10 @@ import app from "./src/app.js";
 
 const PORT = env.PORT;
 
+app.get("/", (req, res) => {
+  res.send('Mon serveur fonctionne bien !!! ')
+})
+
 app.listen(PORT, () => {
   console.log(`Serveur tourn sur localhost:${PORT}`);
 });
