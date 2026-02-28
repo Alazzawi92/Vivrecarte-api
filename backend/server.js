2ff1f1f7d15b0c329937a994d5159c433a703104
wazabi64000 <wazabi64000@gmail.com>
2026-02-28 12:35:41 +0100

    changement dans le server.js et packages.json pour la route get test sur vercel

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,21 @@
 import { env } from "./src/config/env.js";
 import app from "./src/app.js";
 
-const PORT = env.PORT;
+// 1. Configuration des Headers CSP
+app.use((req, res, next) => {
+  res.setHeader("Content-Security-Policy", "default-src 'self'; script-src 'self' 'unsafe-inline';");
+  next();
+});
 
+// 2. Ta route de test
 app.get("/", (req, res) => {
-  res.send('Mon serveur fonctionne bien !!! ')
-})
+  res.send('Mon serveur fonctionne bien sur Vercel !!!');
+});
 
-// Optionnel : On ne lance le .listen() que si on est en local
+// 3. IMPORTANT : Exportation pour Vercel (indispensable en Serverless)
+export default app;
+
+// 4. Lancement local uniquement
 if (process.env.NODE_ENV !== 'production') {
   const PORT = env.PORT || 3000;
   app.listen(PORT, () => {
